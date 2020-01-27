445bf5300321ddab607211d73b66201deb4658b7
koziolmail <koziol_mail@tlen.pl>
2020-01-27 22:55:33 +0100

    link generator

---
+++ LinkCreator/LinkDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkCreator
+{
+    class LinkDto
+    {
+        public string Address { get; set; }
+        public string User { get; set; }
+        public string Pass { get; set; }
+        public string Path { get; set; }
+    }
+}
--- LinkCreator/MainWindow.xaml.cs
+++ LinkCreator/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Downloader
+namespace LinkCreator
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -22,6 +22,7 @@ namespace Downloader
     {
         public MainWindow()
         {
+            DataContext = new MainWindowModel();
             InitializeComponent();
         }
     }
