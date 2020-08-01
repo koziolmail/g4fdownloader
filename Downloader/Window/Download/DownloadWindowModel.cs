9c14a05a9890bd79cbff737fbfcef24aac5ad6d7
pkozlowski <pkozlowski@doz.pl>
2020-08-01 18:15:41 +0200

    disable link input and director choose button while downloading #5

--- Downloader/Window/Download/DownloadWindowModel.cs
+++ Downloader/Window/Download/DownloadWindowModel.cs
@@ -3,13 +3,41 @@ using GalaSoft.MvvmLight.Command;
 using System;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace Downloader {
     class DownloadWindowModel : IMainWindowModelAction {
         private readonly DownloadService DownloadService;
         private readonly ChooseDirectoryService ChooseFolderService;
         private readonly LinkService LinkService;
-
+        private bool isLinkValid = false;
+        private bool IsLinkValid {
+            get { return isLinkValid; }
+            set {
+                isLinkValid = value;
+                ChooseDestinationFolderCommand.RaiseCanExecuteChanged();
+                DownloadCommand.RaiseCanExecuteChanged();
+            }
+        }
+        private bool isDestinationPathSet = false;
+        private bool IsDestinationPathSet {
+            get { return isDestinationPathSet; }
+            set {
+                isDestinationPathSet = value;
+                DownloadCommand.RaiseCanExecuteChanged();
+            }
+        }
+        private bool isBusy = false;
+        public bool IsBusy {
+            get { return isBusy; }
+            set {
+                isBusy = value;
+                Application.Current.Dispatcher.Invoke(() => {
+                    DownloadCommand.RaiseCanExecuteChanged();
+                    ChooseDestinationFolderCommand.RaiseCanExecuteChanged();
+                });
+            }
+        }
         public DownloadWindowBinding Binding { get; private set; }
         public RelayCommand DownloadCommand { get; private set; }
         public RelayCommand ChooseDestinationFolderCommand { get; private set; }
@@ -28,20 +56,15 @@ namespace Downloader {
             if (path != "") {
                 Binding.DestinationPath = path;
                 IsDestinationPathSet = true;
-                DownloadCommand.RaiseCanExecuteChanged();
+
             }
         }
-
         public LinkDto DecalcLink(string base64Link) {
             LinkDto link = LinkService.DecalcLink(base64Link);
             IsLinkValid = link != null;
-            ChooseDestinationFolderCommand.RaiseCanExecuteChanged();
-            DownloadCommand.RaiseCanExecuteChanged();
+
             return link;
         }
-
-        public bool IsBusy { get; set; }
-
         public bool CanDownload() {
             if (!IsLinkValid)
                 return false;
@@ -51,7 +74,6 @@ namespace Downloader {
 
             return !IsBusy;
         }
-
         async void DownloadAsync() {
             string res = await Task.Run(DownloadDo);
             MessageBox.Show(res);
@@ -60,21 +82,21 @@ namespace Downloader {
         async public Task<string> DownloadDo() {
             try {
                 IsBusy = true;
+
                 return await DownloadService.Download(Binding.LinkDto, Binding.DestinationPath, this);
             } finally {
                 IsBusy = false;
             }
         }
-
-        private bool IsLinkValid { get; set; } = false;
-        private bool IsDestinationPathSet { get; set; } = false;
         public bool CanChooseDestinationFolder() {
-            return IsLinkValid;
-        }
+            if (!IsLinkValid)
+                return false;
 
+            return !IsBusy;
+        }
         public void SetProgress(int percentValue, long fileSize, long downloadProgress, string currentFile, int fileInDirNo, int filesinDirCount) {
             Binding.PercentPrograssValue = percentValue;
-            Binding.StringProgressValue = String.Format("{0:n0}kB / {1:n0}kB  - {2}% - plik {3} z {4} - {5}", downloadProgress/1024L, fileSize / 1024L, percentValue, fileInDirNo, filesinDirCount, currentFile);
+            Binding.StringProgressValue = String.Format("{0:n0}kB / {1:n0}kB  - {2}% - plik {3} z {4} - {5}", downloadProgress / 1024L, fileSize / 1024L, percentValue, fileInDirNo, filesinDirCount, currentFile);
         }
     }
 }
